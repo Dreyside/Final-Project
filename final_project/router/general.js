4ac89d1d855cecfa4a05b138fc5d52d1f0cd7f77
Aravind-2010 <mr.aravindsrini@gmail.com>
2023-10-27 17:06:31 -0400

    Book Review Application - All Tasks Completed and the working code is checked with POSTMAN.
    Signed-off-by: Aravind-2010 <mr.aravindsrini@gmail.com>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,7 +25,7 @@ public_users.post("/register", (req,res) => {
 public_users.get('/',function (req, res) {
   //Write your code here
   res.send(JSON.stringify(books, null, 4));
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 
@@ -34,7 +34,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   res.send(books[isbn]);
-  return res.status(300).json({message: "Yet to be implemented"});
+  
  });
   
 // Get book details based on author
